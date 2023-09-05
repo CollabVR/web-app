81658ff667b3392bb9bcfd8d5ee14c9e7eb74007
bygerman <by.german@outlook.com>
2023-09-05 01:34:25 -0500

    feat: Add activities scheduling module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon'
 import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
-import { ActivitiesComponent } from './modules/activity-scheduling/pages/activities/activities.component';
-import { MyActivitiesComponent } from './modules/activity-scheduling/pages/my-activities/my-activities.component';
+import { ActivitySchedulingModule } from './modules/activity-scheduling/activity-scheduling.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
-    ActivitiesComponent,
-    MyActivitiesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    ActivitySchedulingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
