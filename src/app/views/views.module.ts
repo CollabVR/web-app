65f14eada8a533dbefda29ade4debc9827044172
bygerman <by.german@outlook.com>
2023-09-05 03:05:31 -0500

    chore: Add modular Structure

---
+++ src/app/views/views.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageNotFoundView } from './page-not-found/page-not-found.view';
+
+
+
+@NgModule({
+  declarations: [
+    PageNotFoundView,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ViewsModule { }
