c9911faf79e8e9fc0108a433026d50dadf18e463
bygerman <by.german@outlook.com>
2023-09-06 04:07:56 -0500

    feat: add activity-item component

--- src/app/modules/activity-scheduling/activity-scheduling.module.ts
+++ src/app/modules/activity-scheduling/activity-scheduling.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivitiesComponent } from './pages/activities/activities.component';
 import { MyActivitiesComponent } from './pages/my-activities/my-activities.component';
+import { MatCardModule } from '@angular/material/card';
+import { ActivityItemComponent } from './components/activity-item/activity-item.component';
 
 @NgModule({
   declarations: [
     ActivitiesComponent,
     MyActivitiesComponent,
+    ActivityItemComponent,
   ],
   imports: [
     CommonModule,
     MatButtonModule,
+    MatCardModule
   ]
 })
 export class ActivitySchedulingModule { }
---
+++ src/app/modules/activity-scheduling/components/activity-item/activity-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'activity-item',
+  templateUrl: './activity-item.component.html',
+  styleUrls: ['./activity-item.component.css']
+})
+export class ActivityItemComponent {
+
+}
