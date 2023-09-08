f6168df9ebb071d97964e8c585ea081dc4840016
bygerman <by.german@outlook.com>
2023-09-08 02:06:56 -0500

    chore: remove 'modules' folder level

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ActivitySchedulingModule } from './modules/activity-scheduling/activity-scheduling.module';
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
 import { ViewsModule } from './views/views.module';
+import { ActivitySchedulingModule } from './activity-scheduling/activity-scheduling.module';
 
 @NgModule({
   declarations: [
