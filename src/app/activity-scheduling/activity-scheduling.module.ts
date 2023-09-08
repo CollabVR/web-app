7b81eabdbf4d5c5e8206d9e60374da48065eafe5
bygerman <by.german@outlook.com>
2023-09-08 04:02:11 -0500

    feat: add activity-list-component

--- src/app/activity-scheduling/activity-scheduling.module.ts
+++ src/app/activity-scheduling/activity-scheduling.module.ts
@@ -5,12 +5,14 @@ import { ActivitiesComponent } from './pages/activities/activities.component';
 import { MyActivitiesComponent } from './pages/my-activities/my-activities.component';
 import { MatCardModule } from '@angular/material/card';
 import { ActivityItemComponent } from './components/activity-item/activity-item.component';
+import { ActivityListComponent } from './components/activity-list/activity-list.component';
 
 @NgModule({
   declarations: [
     ActivitiesComponent,
     MyActivitiesComponent,
     ActivityItemComponent,
+    ActivityListComponent,
   ],
   imports: [
     CommonModule,
